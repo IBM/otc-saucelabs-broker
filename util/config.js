184b6825e867ab3390c884dd7f433f4d634c6ce1
Magnus Hoglund <magnus.hoglund@se.ibm.com>
2016-01-18 17:04:10 +0100

    tiam stuff to config.js

--- util/config.js
+++ util/config.js
@@ -13,5 +13,13 @@ module.exports = {
 	contextRoot: process.env.CONTEXT_ROOT || config.contextRoot,
 	contextPath: process.env.CONTEXT_PATH || config.contextPath,
 	buildNumber: process.env.BUILD_NUMBER,
-	dashboardUrl: config.dashboardUrl
+	dashboardUrl: config.dashboardUrl,
+
+	tiam_url: process.env.TIAM_URL || config.tiam_url,
+    tiam_client_id: process.env.TIAM_CLIENT_ID || config.tiam_client_id,
+    tiam_client_secret: process.env.TIAM_CLIENT_SECRET  || config.tiam_client_secret,
+
+	cloudant_username: process.env.CLOUDANT_USERNAME || config.cloudant_username,
+	cloudant_password: process.env.CLOUDANT_PASSWORD || config.cloudant_password,
+	db_name: process.env.DB_NAME || config.db_name,
 };
