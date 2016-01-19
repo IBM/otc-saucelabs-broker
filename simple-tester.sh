6a475e4daeca09d9ee70c3d93be5d05c537c5ca4
Magnus Hoglund <magnus.hoglund@se.ibm.com>
2016-01-19 08:24:45 +0100

    Tests in correct order

--- simple-tester.sh
+++ simple-tester.sh
@@ -9,6 +9,8 @@ curl -k -i -X PUT -d'{}' $URL/saucelabs-broker/api/v1/service_instances/123
 
 curl -k -i -X PUT -d'{}' $URL/saucelabs-broker/api/v1/service_instances/123/toolchains/3434
 
+curl -k -i -X DELETE $URL/saucelabs-broker/api/v1/service_instances/123/toolchains/3434
+
 curl -k -i -X DELETE $URL/saucelabs-broker/api/v1/service_instances/123
 
-curl -k -i -X DELETE $URL/saucelabs-broker/api/v1/service_instances/123/toolchains/3434
+
