05c38a03655580ec650ba04ccf7ffd9bb3fa1218
Magnus Hoglund <magnus.hoglund@se.ibm.com>
2016-02-07 12:04:23 +0100

    Added copyright

--- version.sh
+++ version.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2015, 2016. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
 cat <<END > build_info.json                                       
 {                                        
   "time": "$(date +"%Y-%m-%d %H:%M:%S %z")",
