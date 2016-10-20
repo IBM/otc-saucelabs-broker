db81e6fc5d88fc908ce2239335995447a0ccfa2a
Magnus Hoglund <magnus.hoglund@se.ibm.com>
2016-10-20 14:37:01 +0200

    Revert: 62933: Enable CF Diego in YS1 for - otc-saucelabs-broker

--- deploy/deploy-red.sh
+++ deploy/deploy-red.sh
@@ -12,21 +12,8 @@
 
 RED=${CF_APP}-red
 
-if ! cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org/; then
-    error "Error adding CF plugin repo https://plugins.cloudfoundry.org/"
-fi
-
-if ! cf install-plugin -f Diego-Enabler -r CF-Community; then
-    error "Error installing Diego-Enabled plugin"
-fi
-
-
 # push the app
 cf push "$RED" -i "${NUM_INSTANCES:-1}" -d "$DOMAIN" -m 1G --no-route --no-start
-
-if ! cf enable-diego "$RED"; then
-    error "Error enabling diego for $RED"
-fi
 
 # set the otc broker secret
 cf set-env "$RED" OTC_API_BROKER_SECRET "$OTC_API_BROKER_SECRET"
